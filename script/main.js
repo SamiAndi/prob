ae1c2e33033a47d0768fea045a937d77b3c8935b
mohsa <itsmohsaa@gmail.com>
2024-03-09 17:32:59 +0330

    prob

---
+++ script/main.js
@@ -0,0 +1,113 @@
+// Define monaco editor and functionalities
+let editor;
+require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs' } });
+require(['vs/editor/editor.main'], function () {
+    function fontSize() {
+        return window.innerWidth <= 768 ? 18 : 20;
+    }
+
+    editor = monaco.editor.create(document.getElementById('editor-container'), {
+        value: localStorage.PROBLEM || `// احتمال یک آمدن تاس\r\nconst x = random(1, 6)\r\nreturn x == 1`,
+        language: 'javascript',
+        theme: 'vs-dark',
+        minimap: { enabled: false },
+        fontSize: fontSize()
+    });
+
+    editor.onDidChangeModelContent(function () {
+        localStorage.PROBLEM = editor.getValue();
+    });
+
+    $(window).on('resize', function () {
+        editor.layout();
+        editor.updateOptions({ fontSize: fontSize() });
+    });
+
+    $('.spinner-border').parent().remove();
+});
+
+
+// Define worker
+let worker;
+defineWorker();
+
+
+// Define loop input element
+const loopInputElement = $('#loop-input');
+loopInputElement.val(localStorage.LOOP || '');
+
+
+// Start button function
+$("#btn-start").on("click", function () {
+    // Get loop
+    const loopValue = loopInputElement.val();
+    let loop = Number(loopValue);
+    if (!loop || loop < 1) {
+        loopInputElement.focus();
+        return;
+    }
+    localStorage.LOOP = loopValue;
+
+    // Empty table & error, Toggle buttons
+    $('.right-block tbody').html('');
+    $('.error').text('');
+    $('#btn-start').addClass('d-none');
+    $('#btn-stop').removeClass('d-none');
+
+    // Get problem
+    const problem = editor.getValue();
+
+    // Call worker
+    worker.postMessage({ loop, problem });
+});
+
+
+// Stop button function
+$("#btn-stop").on("click", function () {
+    window.location.reload();
+});
+
+
+// tr adder function
+function tableRowAdder(row) {
+    $('.right-block tbody').append(`<tr>${row}</tr>`);
+    const el = $('.right-block');
+    const remainingSpace = el[0].scrollHeight - el.scrollTop() - el.innerHeight();
+    if (remainingSpace < 70) el.scrollTop(el[0].scrollHeight);
+}
+
+
+// toggle buttons after process finished
+function finishToggle() {
+    $('#btn-start').removeClass('d-none');
+    $('#btn-stop').addClass('d-none');
+}
+
+
+// Function to define worker
+function defineWorker() {
+    if (worker) worker.terminate(); // Terminate the existing worker
+    worker = new Worker('script/worker.js'); // Create a new worker
+    worker.onmessage = function (event) {
+        const [type, data] = event.data;
+        if (type === 'result') {
+            const { result, attempt } = data;
+            tableRowAdder(`
+            <td class="text-center">${result}</td>
+            <td class="text-center">${attempt}</td>
+            `);
+        } else if (type === 'finish') {
+            const { result, time } = data;
+            tableRowAdder(`<td colspan="2" class="text-center finish">${result}</td>`);
+            tableRowAdder(`<td colspan="2" class="text-center finish">${time}</td>`);
+            finishToggle();
+            defineWorker();
+        } else if (type === 'error') {
+            $('.error').text(data);
+            finishToggle();
+            defineWorker();
+        } else {
+            window.location.reload();
+        }
+    };
+}
---
+++ script/worker.js
@@ -0,0 +1,91 @@
+const functions = `
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function selectRandomFromArray(arr, k) {
+    const shuffledArray = shuffleArray([...arr]);
+    return shuffledArray.slice(0, k);
+}
+
+function factorial(num) {
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+function combinations(n, k) {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+function permutations(n, k) {
+    return factorial(n) / factorial(n - k);
+}
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+function randomBoolean() {
+    return Math.random() < 0.5;
+}
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+        i += 6;
+    }
+    return true;
+}
+
+function mean(arr) {
+    return arr.reduce((acc, val) => acc + val, 0) / arr.length;
+}
+
+function median(arr) {
+    const sortedArr = sorted(arr);
+    const mid = Math.floor(sortedArr.length / 2);
+    return sortedArr.length % 2 !== 0 ? sortedArr[mid] : (sortedArr[mid - 1] + sortedArr[mid]) / 2;
+}
+
+function sorted(arr) {
+    return arr.slice().sort((a, b) => a - b);
+}
+`
+
+self.onmessage = function (event) {
+    const { loop, problem } = event.data;
+    let wrappedProblem;
+    try {
+        wrappedProblem = (new Function(`${functions}return function(){${problem}}`))();
+        Boolean(wrappedProblem());
+    } catch (err) {
+        self.postMessage(['error', 'Error: ' + err.message]);
+        wrappedProblem = undefined;
+    }
+    if (wrappedProblem) {
+        const sTime = Date.now();
+        let trues = 0;
+        for (let i = 1; i <= 100; i++) {
+            for (let j = 1; j <= loop; j++) trues += Boolean(wrappedProblem());
+            self.postMessage(['result', {
+                result: (trues / loop / i * 100).toFixed(5) + '%',
+                attempt: i
+            }]);
+        }
+        self.postMessage(['finish', {
+            result: `~ ${(trues / loop).toFixed(2)}%`,
+            time: (Date.now() - sTime) / 1e3 + 's'
+        }]);
+    }
+};
